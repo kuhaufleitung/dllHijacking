f9ee00de7641db9e2d1410eb3f440b22da6d8bf6
kuhaufleitung <merk@hm.edu>
2024-12-09 14:36:53 +0100

    remove comments

--- AttackerDLL/dllmain.cpp
+++ AttackerDLL/dllmain.cpp
@@ -14,9 +14,12 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
         CreateThread(nullptr, NULL, (LPTHREAD_START_ROUTINE)startMalicousCode, hModule, NULL, nullptr);
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
+        break;
+    default:
         break;
     }
     return TRUE;
@@ -31,30 +34,24 @@ INT startMalicousCode(HMODULE hModule)
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
 
-    // Create the process
-    // nullptr means use the current environment
-    // nullptr for current directory means use current directory
-    // lpCommandLine needs to be a modifiable string in Windows API
     wchar_t prog[] = TEXT("calc.exe");
     const LPWSTR commandLine = prog;
 
     BOOL success = CreateProcess(
-        nullptr,               // No module name (use command line)
-        commandLine,        // Command line (executable name)
-        nullptr,               // Process handle not inheritable
-        nullptr,               // Thread handle not inheritable
-        FALSE,              // No handle inheritance
-        0,                  // No creation flags
-        nullptr,               // Use parent's environment 
-        nullptr,               // Use parent's starting directory 
-        &si,                // Pointer to STARTUPINFO structure
-        &pi                 // Pointer to PROCESS_INFORMATION structure
+        nullptr,
+        commandLine,
+        nullptr,
+        nullptr,
+        FALSE,
+        0,
+        nullptr,
+        nullptr,
+        &si,
+        &pi
     );
 
-    // Check if process creation was successful
     if (!success)
     {
-        // GetLastError() returns the error code
         DWORD error = GetLastError();
         std::cerr << "Failed to launch calculator. Error code: " << error << "\n";
         return false;
